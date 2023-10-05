3c4b77bfc16b0326bb018b620bae6aa28b8c8bed
Ruskidre <andrey8@pdx.edu>
2023-10-05 16:26:32 -0700

    CSS selectors done, bootstrap changes made in html file, palidrone function works

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,24 @@
-const elem = document.querySelector('input');
-
-elem.addEventListener('input', handleInput);
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElem = document.querySelector('input');
+    const resultDiv = document.querySelector('p + div');
+  
+    const isPalindrome = (number) => {
+      const str = number.toString();
+      return str === str.split('').reverse().join('');
+    };
+  
+    const handleInput = () => {
+      const number = parseInt(inputElem.value, 10);
+  
+      if (isNaN(number) || number < 0) {
+        resultDiv.textContent = 'Please enter a positive number.';
+      } else if (isPalindrome(number)) {
+        resultDiv.textContent = 'Yes, it is a palindrome.';
+      } else {
+        resultDiv.textContent = 'No, it is not a palindrome.';
+      }
+    };
+  
+    inputElem.addEventListener('input', handleInput);
+  });
+  
